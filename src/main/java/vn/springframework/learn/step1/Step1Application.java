aa624d5dc6b04b48bc9546d6608db9c17a760c6d
Nguyen Van Phuc <phucnv9985@yahoo.com>
2018-12-14 11:15:33 +0700

    add init files

---
+++ src/main/java/vn/springframework/learn/step1/Step1Application.java
@@ -0,0 +1,14 @@
+package vn.springframework.learn.step1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step1Application.class, args);
+	}
+
+}
+
